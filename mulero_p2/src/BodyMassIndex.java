5b430cb4c8acec98ba4ccbc1d876ee6dc3451c34
Rey Mulero <reymu96@gmail.com>
2020-09-25 22:54:12 -0400

    program 2 initial commit

---
+++ mulero_p2/src/BodyMassIndex.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.
+
+public class BodyMassIndex {
+    double UserHeight;
+    double UserWeight;
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Please enrter a weight for mesearment in pounds");
+
+
+}
