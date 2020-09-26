95983bc82ad186dd6ed400f505a469d58fef5712
Rey Mulero <reymu96@gmail.com>
2020-09-25 23:46:46 -0400

    second commit

--- mulero_p2/src/App.java
+++ mulero_p2/src/App.java
@@ -1,6 +1,60 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class App {
+
+    private static double getUserHeight() {
+        Scanner in = new Scanner(System.in);
+        double height;
+        System.out.println("Please enter a weight for user in height in inches please");
+        height = in.nextInt();
+        in.nextLine();
+        while( height <0) {
+            System.out.println("you enter a negative number");
+            System.out.println("pleas enter a new number and please make sure it is posative");
+            height = in.nextInt();
+            in.nextLine();
+        }
+
+        if (height > 0){
+            System.out.print("you entered " + height);
+            in.nextLine();
+        }
+
+        return height;
+    }
+
+    private static double getUserWeight() {
+        Scanner in = new Scanner(System.in);
+        double weight;
+        System.out.println("Please enter a weight for user in height in inches please");
+        weight = in.nextInt();
+        in.nextLine();
+        while( weight <0)
+        {
+            System.out.println("you enter a negative number");
+            System.out.println("pleas enter a new number and please make sure it is posative");
+            weight = in.nextInt();
+            in.nextLine();
+        }
+        if (weight > 0){
+            System.out.print("you entered " + weight);
+            in.nextLine();
+        }
+
+        return weight;
+    }
+
+    private static void displayBmiInfo(BodyMassIndex bmi) {
+    }
+
+    private static boolean moreInput() {
+    }
+
+    private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+    }
 
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
@@ -11,10 +65,17 @@ public class App {
 
             BodyMassIndex bmi = new BodyMassIndex(height, weight);
             bmiData.add(bmi);
-
+            System.out.println();
             displayBmiInfo(bmi);
         }
 
         displayBmiStatistics(bmiData);
     }
+
+
+
 }
+
+
+
+
--- mulero_p2/src/BodyMassIndex.java
+++ mulero_p2/src/BodyMassIndex.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
-import java.lang.
-
-public class BodyMassIndex {
-    double UserHeight;
-    double UserWeight;
-
-    Scanner in = new Scanner(System.in);
-
-    System.out.println("Please enrter a weight for mesearment in pounds");
 
 
+public class BodyMassIndex(){
+
+    BodyMassIndex(double height, double weight){
+    }
 }
+
+
