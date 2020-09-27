386c604cc91ff9755cd26ef22b2c77dfa3d438fe
Rey Mulero <reymu96@gmail.com>
2020-09-27 02:04:02 -0400

    finished app and bmi class

--- mulero_p2/src/App.java
+++ mulero_p2/src/App.java
@@ -4,56 +4,87 @@ import java.util.Scanner;
 
 
 public class App {
+    private static double Height;
+    private static double Weight;
+    private static ArrayList<Double> Scores = new ArrayList<Double>();
 
     private static double getUserHeight() {
         Scanner in = new Scanner(System.in);
-        double height;
-        System.out.println("Please enter a weight for user in height in inches please");
-        height = in.nextInt();
+        System.out.println("Please enter a height for user in height in inches please");
+        Height = in.nextInt();
         in.nextLine();
-        while( height <0) {
+        while( Height <0) {
             System.out.println("you enter a negative number");
-            System.out.println("pleas enter a new number and please make sure it is posative");
-            height = in.nextInt();
+            System.out.println("pleas enter a new number and please make sure it is positive");
+            Height = in.nextInt();
             in.nextLine();
         }
 
-        if (height > 0){
-            System.out.print("you entered " + height);
+        if (Height > 0){
+            System.out.print("you entered " + Height);
             in.nextLine();
         }
 
-        return height;
+        return Height;
     }
 
     private static double getUserWeight() {
         Scanner in = new Scanner(System.in);
-        double weight;
-        System.out.println("Please enter a weight for user in height in inches please");
-        weight = in.nextInt();
+
+        System.out.println("Please enter a weight for user in weight in pounds please");
+        Weight = in.nextInt();
         in.nextLine();
-        while( weight <0)
+        while( Weight <0)
         {
             System.out.println("you enter a negative number");
             System.out.println("pleas enter a new number and please make sure it is posative");
-            weight = in.nextInt();
+            Weight = in.nextInt();
             in.nextLine();
         }
-        if (weight > 0){
-            System.out.print("you entered " + weight);
+        if (Weight > 0){
+            System.out.print("you entered " + Weight);
             in.nextLine();
         }
 
-        return weight;
+        return Weight;
     }
 
     private static void displayBmiInfo(BodyMassIndex bmi) {
+        double bmiscore;
+        String BmiCategory;
+        bmiscore = bmi.BmiCalculation(Height,Weight);
+        System.out.printf("Your bmi is %.1f \n", bmiscore);
+        Scores.add(bmiscore);
+        BmiCategory = bmi.BmiCategory(bmiscore);
+        System.out.println("with a category of "+ BmiCategory);
+        System.out.println();
     }
 
     private static boolean moreInput() {
+        Scanner in = new Scanner(System.in);
+        boolean answer;
+        System.out.println("would you like to enter a new set of measurements for calculations\n" +
+                "type Y for yes or N for no");
+        String YesOrNo = in.nextLine();
+        if (YesOrNo.equals("Y")){
+            answer = true;
+        }
+        else {
+            answer = false;
+        }
+        return answer;
     }
 
     private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+         int count = 0;
+         double total = 0;
+        for (double score:Scores) {
+           total += score;
+           count++;
+        }
+        double average = 0;
+        average = total/count;
+        System.out.printf("The average of all bmi enter is %.1f \n",average);
     }
 
     public static void main(String[] args) {
--- mulero_p2/src/BodyMassIndex.java
+++ mulero_p2/src/BodyMassIndex.java
@@ -1,10 +1,34 @@
-import java.util.Scanner;
+public class BodyMassIndex {
 
+    public double BmiCalculation( double inches , double pounds){
+       double Bmi = 0;
+       Bmi =(703 * pounds / Math.pow(inches,2));
+       return Bmi;
+    }
+    public String BmiCategory(double Bmi){
+        String bmiCategory = "";
+        if (Bmi < 18.5){
+            bmiCategory= "underweight";
+        }
+        else if ((Bmi >=18.5) && (Bmi <= 24.99)){
+            bmiCategory = "normal";
 
-public class BodyMassIndex(){
+        }
+        else if ((Bmi>= 25 )&& (Bmi <= 29.99)){
+            bmiCategory = "overweight";
+        }
+        else if (Bmi >= 30) {
+            bmiCategory = "Obesity";
+        }
+        return bmiCategory;
+    }
 
     BodyMassIndex(double height, double weight){
+        double bmi;
+        bmi = BmiCalculation(height, weight);
+        BmiCategory(bmi);
     }
+
 }
 
 
