a979d3e8256517c1a8f18121f9b952313bc193a6
Rey Mulero <reymu96@gmail.com>
2020-09-27 22:03:28 -0400

    created all the nesecary test cases for my test file (currently empty test cases to be filled in)

--- mulero_p2/src/BodyMassIndexTest.java
+++ mulero_p2/src/BodyMassIndexTest.java
@@ -1,3 +1,65 @@
+import org.junit.jupiter.api.Test;
+
 public class BodyMassIndexTest {
 
+    @Test
+    public void TestUnderWightBmiCalulation() {
+        
+
+    }
+
+    @Test
+    public void TestNormalBmiCalulation() {
+
+
+    }
+
+    @Test
+    public void TestOverWightBmiCalulation() {
+
+
+    }
+
+    @Test
+    public void TestObisityBmiCalulation() {
+
+
+    }
+
+    @Test
+    public void TestUnderWightBmiCategory() {
+
+
+    }
+
+    @Test
+    public void TestNormalBmiCategory() {
+
+
+    }
+
+    @Test
+    public void TestOverWightBmiCategory() {
+
+
+    }
+
+    @Test
+    public void TestObesityBmiCategory() {
+
+
+    }
+
+    @Test
+    public void TestBodyMassIndexMethod(){
+
+
+    }
+
+
+
+
+
 }
+
+
