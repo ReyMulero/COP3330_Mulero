c2ef66bb50c6f2e93a7480c89ed410fa7748c589
Rey Mulero <reymu96@gmail.com>
2020-11-13 13:04:03 -0500

    got the add taak requirment to creat a new task finished in there most basic form

--- mulero_p4/src/App.java
+++ mulero_p4/src/App.java
@@ -10,11 +10,14 @@ private static final Scanner choice = new Scanner(System.in);
             switch (optionPickFromMainMenu){
                 case 1:
                     System.out.println("you have created a new list");
-                    TaskList.taskListOptions();
                     exitLoop = true;
+                    TaskList.taskListOptions();
+                    // TaskItem.dueDate();
+                    //TaskItem.description();
+                    //TaskItem.title();
                     break;
                 case 2:
-                    System.out.println("you have choose to look at a your preexisting list");
+                    System.out.println("you have choose to look at a your previous created list");
                     exitLoop = true;
                     break;
                 case 3:
@@ -27,15 +30,15 @@ private static final Scanner choice = new Scanner(System.in);
                     optionPickFromMainMenu = choice.nextInt();
                     choice.nextLine();
             }
-        }while(exitLoop);
+        }while(!exitLoop);
 
     }
 
     public static void mainMenu(){
         System.out.println("---------------main menu------------------\n");
         System.out.println("1) create a new list\n" +
-                "2) view created list\n" +
-                "3) exit program");
+                           "2) view created list\n" +
+                           "3) exit program");
         System.out.println("\n------------------------------------------\n");
         System.out.println("please enter a number from 1-3");
         int mainMenuChoice;
--- mulero_p4/src/TaskItem.java
+++ mulero_p4/src/TaskItem.java
@@ -1,25 +1,63 @@
 import java.util.Scanner;
 
 public class TaskItem {
-    Scanner input = new Scanner(System.in);
-    private String getTitle(){
+    private static final Scanner input = new Scanner(System.in);
+
+    private static String setTitle(){
         String title;
         title = input.nextLine();
         return title;
 
     }
-    private String title(){
+    public static String title(){
         String taskItemName;
         System.out.println("please enter a title for your task you have created");
-       taskItemName = getTitle();
+       taskItemName = setTitle();
        return taskItemName;
     }
     // An task item shall contain a title
     //a title shall be 1 or more characters in length
 
+    private  static String setDiscription(){
+        String description;
+        description = input.nextLine();
+        return description;
+}
 
+    public  static String description(){
+        System.out.println("please enter a description for the task:");
+        String descript;
+        descript = setDiscription();
+        return descript;
+    }
     //An task item shall contain a description
     //A description shall be 0 or more characters in length
+   private  static String setDueDate(){
+     //   int day;
+       // int month;
+        //int year;
+       String DueDaySet;
+       System.out.println("please enter a due date in the format of YYYY-MM-DD" +
+               "please enter the dates with a space dashes are not needed");
+     //  year = input.nextInt();
+       //month =input.nextInt();
+       //day = input.nextInt();
+
+     DueDaySet = input.nextLine();
+
+     //  DueDaySet = year + "-" + month+ "-" + day;
+       System.out.println(DueDaySet);
+       return DueDaySet;
+   }
+
+
+    public static String dueDate(){
+        System.out.println("All new task require a due date\n" +
+                "due date should be in the form of YYYYY-MM-DD: ");
+        String dueDate;
+        dueDate =  setDueDate();
+       return dueDate;
+    }
     //An task item shall contain a due date
     //A due date shall be in the format of YYYY-MM-DD
 
